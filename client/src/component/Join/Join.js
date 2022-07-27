ad2fd4d99e0beba2cf355a38714878603445af19
Shivam Shukla <77653717+shivam910@users.noreply.github.com>
2022-07-27 22:06:02 +0530

    Update Join.js

--- client/src/component/Join/Join.js
+++ client/src/component/Join/Join.js
@@ -1,23 +1,26 @@
 import "./Join.css";
 import Logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 let user;
 
+const sendUser = () => {
+  user = document.getElementById("joinInput").value
+  document.getElementById("joinInput").value = "";
+}
+
 const Join = () => {
 
-    const sendUser = () => {
-        user = document.getElementById("joinInput").value
-        document.getElementById("joinInput").value = "";
-    }
+    const [ name, setName ] = useState("");
 
   return (
     <div className="JoinPage">
       <div className="JoinContainer">
         <img src={Logo} alt="Logo" />
         <h1>Hey Chat</h1>
-        <input type="text" placeholder="enter username" id="joinInput" />
-        <Link to = "/chat">
+        <input onChange={(e) => setName(e.target.value)} type="text" placeholder="enter username" id="joinInput" />
+        <Link onClick={(event)=> !name ? event.preventDefault():null} to = "/chat">
           <button onClick={sendUser} className="joinBtn">Login</button>
         </Link>
       </div>
